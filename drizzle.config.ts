24b60edadf781b478b4d711fcd79e3f0bf603222
Rjshakya <rajkamalshakya07@gmail.com>
2025-09-07 22:10:38 +0000

    removed drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  out: "./drizzle",
-  schema: "./app/db/schema",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: "postgres://postgres:password@localhost:5432/next-formly",
-  },
-});
